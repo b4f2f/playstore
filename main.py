72e301172b67b4cfbb3cef144ec30c8732211a14
Eminhan <96875856+b4f2f@users.noreply.github.com>
2022-01-29 15:07:56 +0400

    Update main.py

--- main.py
+++ main.py
@@ -35,11 +35,11 @@ async def search(bot, update):
     answers = []
     for result in results:
         details = "**Title:** `{}`".format(result["title"]) + "\n" \
-        "**Description:** `{}`".format(result["description"]) + "\n" \
-        "**App ID:** `{}`".format(result["app_id"]) + "\n" \
-        "**Developer:** `{}`".format(result["developer"]) + "\n" \
-        "**Developer ID:** `{}`".format(result["developer_id"]) + "\n" \
-        "**Score:** `{}`".format(result["score"]) + "\n" \
+        "**Açıklama:** `{}`".format(result["description"]) + "\n" \
+        "**Uygulama Kimliği:** `{}`".format(result["app_id"]) + "\n" \
+        "**Geliştirici:** `{}`".format(result["developer"]) + "\n" \
+        "**Geliştirici Kimliği:** `{}`".format(result["developer_id"]) + "\n" \
+        "**Puan:** `{}`".format(result["score"]) + "\n" \
         "**Price:** `{}`".format(result["price"]) + "\n" \
         "**Full Price:** `{}`".format(result["full_price"]) + "\n" \
         "**Free:** `{}`".format(result["free"]) + "\n" \
